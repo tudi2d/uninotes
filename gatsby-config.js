4c6b49f3ef0f7164cfcb994c0c7fa34427ceba08
Philipp Hugenroth <phugenroth@gmail.com>
2019-05-12 16:31:18 +0200

    Adjust project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Notes Starter',
+    title: "Uni Notes",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
       },
     },
     {
@@ -29,15 +29,15 @@ module.exports = {
               maxWidth: 650,
             },
           },
-        ]
-      }
+        ],
+      },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages'
-      }
-    }
+        name: "pages",
+      },
+    },
   ],
-}
+};
