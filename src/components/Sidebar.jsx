5482c8b9d8325d7bfd49e3b942a2e057f9c506d9
Philipp Hugenroth <phugenroth@gmail.com>
2019-06-12 23:18:00 +0200

    Changed 404 page to Markdown

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,7 @@
 import { Link } from "gatsby";
-import Slugger from "github-slugger";
-import React, { useEffect, useState } from "react";
-
+import React, { useState } from "react";
 import "./sidebar.css";
 
-const slug = new Slugger();
 let articles = [];
 let subjects = [];
 
@@ -18,26 +15,36 @@ const addSubjects = function(edges) {
     headings = headings.map(heading => {
       return {
         ...heading,
-        route: `${route}#${slug.slug(heading.value.toLowerCase(), true)}`,
+        route: `${route}#${heading.value
+          .toLowerCase()
+          .split(" ")
+          .join("-")
+          .split("'")
+          .join("")}`,
       };
     });
-    const page = {
-      title: frontmatter.title ? frontmatter.title : topics[topics.length - 1],
-      headings,
-      route,
-    };
-    let subjectExists = false;
-    subjects.forEach(({ title, pages }) => {
-      if (title === topics[0]) {
-        pages.push(page);
-        subjectExists = true;
-      }
-    });
-    if (!subjectExists) {
-      if (topics[0]) {
-        subjects.push({ title: topics[0], pages: [page] });
-      } else {
-        articles.push(page);
+    if (route !== "/404/") {
+      const page = {
+        title: frontmatter.title
+          ? frontmatter.title
+          : topics[topics.length - 1],
+        headings,
+        route,
+      };
+      console.log(page);
+      let subjectExists = false;
+      subjects.forEach(({ title, pages }) => {
+        if (title === topics[0]) {
+          pages.push(page);
+          subjectExists = true;
+        }
+      });
+      if (!subjectExists) {
+        if (topics[0]) {
+          subjects.push({ title: topics[0], pages: [page] });
+        } else {
+          articles.push(page);
+        }
       }
     }
   });
@@ -47,24 +54,26 @@ const Sidebar = props => {
   const { edges } = props.data.allMarkdownRemark;
   addSubjects(edges);
 
-  useEffect(() => {
-    return () => {
-      slug.reset();
-    };
-  }, [props.location.href]);
   const defaultSidebarShown = true;
   const [shown, toggleSidebar] = useState(defaultSidebarShown);
 
   return (
     <nav className={shown ? "sidebar sidebar-open" : "sidebar sidebar-closed"}>
       <div
-        className={shown ? "sidebar-icon-open" : "sidebar-icon-closed"}
+        className="sidebar-icon-wrapper"
         onClick={() => toggleSidebar(isShown => !isShown)}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <svg
+          className={shown ? "sidebar-icon-open" : "sidebar-icon-closed"}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
           <g>
             <path fill="none" d="M0 0h24v24H0z" />
-            <path d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z" />
+            <path
+              fill="#fff"
+              d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z"
+            />
           </g>
         </svg>
       </div>
@@ -75,7 +84,7 @@ const Sidebar = props => {
               {title}
             </Link>
           </li>
-          <ul className="sidbar-anker">
+          <ul className="sidebar-anker">
             {headings.map(({ value, depth, route }) => (
               <Link to={route} className="link" key={route}>
                 <li className="sidebar-post-headings" key={title + value}>
@@ -90,16 +99,19 @@ const Sidebar = props => {
         <ul className="sidebar-topic" key={title ? title : i}>
           <li>{title ? title.toUpperCase() : ""}</li>
           {pages.map(({ title, route, headings }) => (
-            <ul className="sidebar-article" key={title + route}>
-              <li className="sidebar-post-list">
+            <ul className="sidebar-topic-article" key={title + route}>
+              <li className="sidebar-topic-post-list">
                 <Link to={route} className="link">
                   {title}
                 </Link>
               </li>
-              <ul className="sidbar-anker">
+              <ul className="sidebar-anker">
                 {headings.map(({ value, depth, route }) => (
                   <Link to={route} className="link" key={route}>
-                    <li className="sidebar-post-headings" key={title + value}>
+                    <li
+                      className="sidebar-topic-post-headings"
+                      key={title + value}
+                    >
                       {value}
                     </li>
                   </Link>
--- src/pages/SS19/fosap.md
+++ src/pages/SS19/fosap.md
@@ -1,24 +1,50 @@
 ---
 title: "Formale Systeme, Automaten & Prozesse"
-description: This post is related to the gatsbyjs
-date: "2018-09-26"
 ---
 
 Dies ist meine persönliche Zusammenfassung und Mitschrift der FoSAP Vorlesung des Sommersemesters 2019. Ich beziehe mich hierbei auf die Folien der Vorlesung und Übung und
 den [Panikzettel](https://panikzettel.philworld.de/fosap.pdf) zur Vorlesung.
 
-##DFA's
+## DFA's
 
-Ein deterministische endliche Automat A ist ein 5 Tupel mit $$ A = (Q,\Sigma,\delta,q_0,F) $$, dass sich wie folgt zusammensetzt:
+Ein deterministische endliche Automat A ist ein 5 Tupel mit $A = (Q,\Sigma,\delta,q_0,F)$, dass sich wie folgt zusammensetzt:
 
-- $$ Q $$: Anzahl der Zustände des Automaten
-- $$ \Sigma $$: Eingabealphabet
-- $$ \delta $$: Transitionsfunktion $$ \delta : Q \times \Sigma \rightarrow Q $$
-- $$ q_0 \in Q$$: Startzustand
-- $$ F \subseteq Q$$: Menge der Endzustände
+- $Q$ : Anzahl der Zustände des Automaten
+- $\Sigma$ : Eingabealphabet
+- $\delta$: Transitionsfunktion $\delta : Q \times \Sigma \rightarrow Q$
+- $q_0 \in Q$: Startzustand
+- $F \subseteq Q$: Menge der Endzustände
 
-##NFA's
+Ein Lauf auf einem Automaten ist ein Tupel der Form $(r_0,a_1,r_1,a_2,...,a_n,r_n)$ mit $\delta (r_{i-1},a_i) = r_i$ und $r_0 = q_0$. Der Lauf ist genau dann akzeptierend, wenn $r_n \in F$.
 
-##Reguläre Sprachen
+Ein Automat erkennt eine Sprache $L(A) := \{ w \in \Sigma^* | A \text{ akzeptiert } w \}$. Allgemein ist eine Sprache L DFA-erkennbar, wenn es einen DFA gibt mit $L=L(A)$.
 
-##TEST
+Für alle Sprachen gilt:
+
+- Assoziativ: $(KL)M = K(LM)$
+- $L \{ \epsilon \} = \{ \epsilon \}L = L$
+- $L \emptyset = \emptyset L = \emptyset$
+- Distributiv: $K(L \cup M) = KL \cup KM$ und $(K \cup L)M = KM \cup LM$
+- $L^0 = \{ \epsilon \}$
+- $L^{n+1} := L^n L$ für $n \in N$
+- Kleene-Stern: $L^* := \underset{n \in N}{\bigcup} L^n$
+- $L^* L^* = L^*$
+- $(L^*)^* = L^*$
+- $\emptyset^* = \{\epsilon\}$
+
+### Produktautomaten
+
+Seien 2 DFA's $A_1, A_2$ mit dem selben Alphabet gegeben. Dann ist der Produktautomat:
+$A=(Q_1 \times Q_2, \Sigma, \delta, (q_{01},q_{02}),F)$.
+
+## NFA's
+
+## Rekursion und Induktion
+
+## Reguläre Sprachen
+
+## Algorithmen für Reguläre Sprachen
+
+## Automaten mit Ausgabe
+
+## Kontextfreie Sprachen
